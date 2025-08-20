f3b606f8f27cc14aa4b2104e8f8d996c4e1e764f
Roger Calderón <rhost.cc@gmail.com>
2025-08-19 21:56:36 -0400

    Estructura lista simple insertAt

--- AppListaSimple/Program.cs
+++ AppListaSimple/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AppListaSimple.clases;
 
 namespace AppListaSimple
 {
@@ -10,6 +11,18 @@ namespace AppListaSimple
     {
         static void Main(string[] args)
         {
+            ListaSimple<int> miLista = new ListaSimple<int>();
+            miLista.InsertAt(0, 1);
+            miLista.InsertAt(1, 2);
+            miLista.InsertAt(2, 3);
+            miLista.InsertAt(3, 4);
+            miLista.InsertAt(4, 5);
+            miLista.InsertAt(5, 6);
+
+            miLista.InsertAt(1, 7);
+            miLista.InsertAt(5, 8);
+            Console.WriteLine(miLista.ToString());
+
         }
     }
 }
--- AppListaSimple/clases/ListaSimple.cs
+++ AppListaSimple/clases/ListaSimple.cs
@@ -11,24 +11,88 @@ namespace AppListaSimple.clases
     {
         private NodoSimple<T> _root;
         private int _count;
+        public ListaSimple()
+        {
+            Clear();
+        }
         public void Clear()
         {
-            throw new NotImplementedException();
+            this._root = null;
+            this._count = 0;
         }
 
         public int Count()
         {
-            throw new NotImplementedException();
+            return this._count;
         }
 
         public void InsertAt(int index, T element)
         {
-            throw new NotImplementedException();
+            NodoSimple<T> newNodo = new NodoSimple<T>(element);
+            if (index < 0 || index > this._count)
+                throw new IndexOutOfRangeException("Indice incorrecto");
+
+            // Insertar en lista vacia
+            if(IsEmpty())
+            {
+                this._root = newNodo;
+                this._count++;
+                return;
+            }
+
+            // Insertar al Inicio
+            if(index == 0)
+            {
+                newNodo.Next = this._root;
+                this._root = newNodo;
+                this._count++;
+                return;
+            }
+            // Insertar en medio
+            if(index > 0 && index < this._count)
+            {
+                int currentIndex = 0;
+                NodoSimple<T> temp = this._root;
+                while(currentIndex < index - 1)
+                {
+                    temp = temp.Next;
+                    currentIndex++;
+                }
+                newNodo.Next = temp.Next;
+                temp.Next = newNodo;
+                this._count++;
+                return;
+            }
+
+            // Insertar al final
+            if (index == this._count)
+            {
+                NodoSimple<T> temp = this._root;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = newNodo;
+                this._count++;
+                return;
+            }
+        }
+        public override string ToString()
+        {
+            string result = "";
+            NodoSimple<T> temp = this._root;
+            while (temp.Next != null)
+            {
+                result = result + $"({temp.Value})->";
+                temp = temp.Next;
+            }
+            result = result + $"({temp.Value})->null";
+            return result;
         }
 
         public bool IsEmpty()
         {
-            throw new NotImplementedException();
+            return this._root == null;
         }
 
         public T RemoveAt(int index)
