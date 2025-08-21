bb5b1a4448cf6545f8140b6c05d4d54d2817ba59
Roger Calderón <rhost.cc@gmail.com>
2025-08-20 20:30:39 -0400

    Lista simple terminada

--- AppListaSimple/Program.cs
+++ AppListaSimple/Program.cs
@@ -14,13 +14,17 @@ namespace AppListaSimple
             ListaSimple<int> miLista = new ListaSimple<int>();
             miLista.InsertAt(0, 1);
             miLista.InsertAt(1, 2);
-            miLista.InsertAt(2, 3);
-            miLista.InsertAt(3, 4);
-            miLista.InsertAt(4, 5);
-            miLista.InsertAt(5, 6);
+            //miLista.InsertAt(2, 3);
+            //miLista.InsertAt(3, 4);
+            //miLista.InsertAt(4, 5);
+            //miLista.InsertAt(5, 6);
+            //miLista.InsertAt(6, 7);
+            //miLista.InsertAt(7, 8);
+            Console.WriteLine(miLista.ToString());
 
-            miLista.InsertAt(1, 7);
-            miLista.InsertAt(5, 8);
+            //miLista.RemoveAt(0); // Eliminando el primero
+            //miLista.RemoveAt(miLista.Count() - 1); // Eliminando el ultimo
+            miLista.RemoveAt(1); // Eliminando el segundo
             Console.WriteLine(miLista.ToString());
 
         }
--- AppListaSimple/clases/ListaSimple.cs
+++ AppListaSimple/clases/ListaSimple.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using AppListaSimple.interfaces;
 
 namespace AppListaSimple.clases
@@ -20,12 +21,10 @@ namespace AppListaSimple.clases
             this._root = null;
             this._count = 0;
         }
-
         public int Count()
         {
             return this._count;
         }
-
         public void InsertAt(int index, T element)
         {
             NodoSimple<T> newNodo = new NodoSimple<T>(element);
@@ -89,20 +88,56 @@ namespace AppListaSimple.clases
             result = result + $"({temp.Value})->null";
             return result;
         }
-
         public bool IsEmpty()
         {
             return this._root == null;
         }
-
         public T RemoveAt(int index)
         {
-            throw new NotImplementedException();
-        }
+            T value;
+            if (index < 0 || index > this._count - 1)
+                throw new IndexOutOfRangeException("Indice incorrecto");
 
+            // Eliminar en lista vacia
+            if (IsEmpty())
+            {
+                throw new IndexOutOfRangeException("La lista esta vacia");
+            }
+
+            // Eliminar al Inicio
+            if (index == 0)
+            {
+                value = this._root.Value;
+                this._root = this._root.Next;
+                this._count--;
+                return value;
+            }
+           
+            int currentIndex = 0;
+            NodoSimple<T> temp = this._root;
+            while (currentIndex != index - 1)
+            {
+                temp = temp.Next;
+                currentIndex++;
+            }
+            value = temp.Next.Value;
+            temp.Next = temp.Next.Next;
+            this._count--;
+            return value;
+        }
         public void UpdateAt(int index, T element)
         {
-            throw new NotImplementedException();
+            if (index < 0 || index > this._count - 1)
+                throw new IndexOutOfRangeException("Indice incorrecto");
+
+            NodoSimple<T> temp = this._root;
+            int currentIndex = 0;
+            while(currentIndex != index)
+            {
+                temp = temp.Next;
+                currentIndex++;
+            }
+            temp.Value = element;
         }
     }
 }
